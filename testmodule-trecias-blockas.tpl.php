ddd346ec946b4c087e108a1320d366a6aee98877
Teo Vitkauskas <tvitkauskas@doubleprime.com>
2013-01-16 15:42:58 -0500

    fixed css filename on tpl file

--- testmodule-trecias-blockas.tpl.php
+++ testmodule-trecias-blockas.tpl.php
@@ -6,7 +6,7 @@
  * @ingroup themeable
  */
 
-drupal_add_css(drupal_get_path('module', 'testmodule') . '/css/testmodule_style.css');
+drupal_add_css(drupal_get_path('module', 'testmodule') . '/css/testmodule-style.css');
 drupal_add_js(drupal_get_path('module', 'testmodule') . '/js/steps.js');  
 ?>
 <div id="step1-bv"></div><div id="step2-bv"></div><div id="step3-bv"></div>
