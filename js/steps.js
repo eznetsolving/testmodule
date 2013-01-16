b17dd01e051798eeac5783bb301fed11383bb923
Teo Vitkauskas <tvitkauskas@doubleprime.com>
2013-01-16 14:46:49 -0500

    added trecias blokas and template theming

---
+++ js/steps.js
@@ -0,0 +1,37 @@
+(function ($) {
+  // API Call configuration
+Drupal.behaviors.testmoduleSteps = {
+  attach: function (context) {
+    var api_server = "http://aveda.ugc.bazaarvoice.com/data";
+    var api_key = "ira2dftpe0uo9wvh4fw7e16pb";
+    var num_items = 3; 
+    function productOutput(product) {
+      var output = '<div class="review-wrapper">';
+      var ReviewProduct = product;
+      var averageRating = 100*(ReviewProduct.ReviewStatistics.AverageOverallRating/5);               
+      output += '<div class="product_name"><a href="' + ReviewProduct.ProductPageUrl + '" target="_blank">' + ReviewProduct.Name + '</a></div>';
+      output += '<div class="average_rating"><span class="rating"><span class="ratingHighlight" style="width:' + averageRating + '%"></span></span><span class="review-count"><a href="' + ReviewProduct.ProductPageUrl + '" target="_blank"> (' + ReviewProduct.TotalReviewCount + ' ' + Drupal.t('reviews') + ')</a></span></div>';
+      return output;      
+    }    
+    $.ajaxSetup({ cache: true });
+    $.getJSON(api_server+"/products.json?callback=?", "apiversion=5.3&passkey=" + api_key + "&filter=id:21056&stats=reviews",
+      function(json){
+        jQuery.each(json.Results, function(index, product) {
+          $("#step1").append(productOutput(product));
+        });
+      });
+      $.getJSON(api_server+"/products.json?callback=?", "apiversion=5.3&passkey=" + api_key + "&filter=id:21057&stats=reviews",
+      function(json){
+        jQuery.each(json.Results, function(index, product) {
+          $("#step2").append(productOutput(product));
+        });
+      });
+      $.getJSON(api_server+"/products.json?callback=?", "apiversion=5.3&passkey=" + api_key + "&filter=id:21058&stats=reviews",
+      function(json){
+        jQuery.each(json.Results, function(index, product) {
+          $("#step3").append(productOutput(product));
+        });
+      });
+  }
+};  
+})(jQuery);
--- testmodule-trecias-blockas.tpl.php
+++ testmodule-trecias-blockas.tpl.php
@@ -1,8 +1,13 @@
 <?php
 /**
-* @file
-*Wraps a formatted <strike> tag around a string.
-*
-*/
+ * @file
+ * Default theme implementation to provide an HTML container for 'Trecias blokas' block.
+ *
+ * @ingroup themeable
+ */
+
+drupal_add_css(drupal_get_path('module', 'testmodule') . '/css/style.css');
+drupal_add_js(drupal_get_path('module', 'testmodule') . '/js/steps.js');  
 ?>
-<strike class="templeitas"><?php $string; ?></strike>
+<div id="step1"></div><div id="step2"></div><div id="step3"></div>
+<p>veikia</p>
